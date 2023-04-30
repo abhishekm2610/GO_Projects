df5f7a67364060cc147b393009861dbb0ae6a11a
Abhishek <abhishekm2610@gmail.com>
2023-04-30 21:00:51 +0530

    Climbing stairs

--- Blind 75/Dynamic Programming/climbingStairs.go
+++ Blind 75/Dynamic Programming/climbingStairs.go
@@ -18,26 +18,28 @@ func climbStairsdP(n int) int {
 	return left
 }
 
-func dfs(current int, dp map[int]int) int {
-	if current == 0 {
+func dfs(current int, length int, dp map[int]int) int {
+	if current == length {
 		return 1
-	} else if current < 0 {
+	} else if current > length {
 		return 0
 	} else if _, isPresent := dp[current]; isPresent {
 		return dp[current]
 	}
-	dp[current] = dfs(current-1, dp) + dfs(current-2, dp)
+	dp[current] = dfs(current+1, length, dp) + dfs(current+2, length, dp)
 	return dp[current]
 }
 func climbStairs(n int) int {
 
+	ans := 0
 	dp := make(map[int]int)
+	ans = dfs(1, n, dp) + dfs(2, n, dp)
 
-	return dfs(n-1, dp) + dfs(n-2, dp)
+	return ans
 }
 
 func main() {
-	n := 4
+	n := 2
 	fmt.Println(climbStairs(n))
 
 }
