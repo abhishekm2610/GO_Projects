165b4ccbb9d7ec1cfe857a72412311ec164ea66f
Abhishek <abhishekm2610@gmail.com>
2023-05-14 01:35:44 +0530

    Solved Bi weekly

--- Contest/13 May Bi Weekly/Q3.go
+++ Contest/13 May Bi Weekly/Q3.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func maxInSlice(nums []int) int {
@@ -36,8 +37,31 @@ func min(a int, b int) int {
 	return b
 }
 
+//	func sumOfArray(arr []int) int {
+//		ans := 0
+//		for _, v := range arr {
+//			ans = ans | v
+//		}
+//		return ans
+//	}
 func maximumOr(nums []int, k int) int64 {
+	multipler := math.Pow(float64(2), float64(k))
+	maxVal := 0
+	preFix := make([]int, len(nums))
+	sufFix := make([]int, len(nums))
 
+	for i := 1; i < len(preFix); i++ {
+		preFix[i] = preFix[i-1] | nums[i-1]
+	}
+	for i := len(sufFix) - 2; i >= 0; i-- {
+		sufFix[i] = sufFix[i+1] | nums[i+1]
+	}
+	for i := 0; i < len(nums); i++ {
+		fmt.Println(preFix[i] | nums[i]*int(multipler) | sufFix[i])
+
+		maxVal = max(maxVal, preFix[i]|nums[i]*int(multipler)|sufFix[i])
+	}
+	return int64(maxVal)
 }
 
 func main() {
--- Contest/13 May Bi Weekly/Q4.go
+++ Contest/13 May Bi Weekly/Q4.go
@@ -40,38 +40,21 @@ func min(a int, b int) int {
 
 func sumOfPower(nums []int) int {
 	sort.Ints(nums)
-	fmt.Println(nums)
-	// multipler := math.Pow(float64(2), float64(k))
-	power := 0
-	l := 0
-	r := 0
-	for l < len(nums) {
-		if r == len(nums) {
-			l++
-			r = len(nums) - 1
-			continue
-		}
-		minPower := nums[l]
-		maxPower := nums[r]
-		chars := int(math.Pow(float64(2), float64(r-l-1)))
-		if l == r {
-			power += (maxPower * maxPower * minPower)
-			power %= ((int(math.Pow(float64(10), float64(9)))) + 7)
-
-		} else {
-			power += ((maxPower * maxPower * minPower) * chars)
-			power %= ((int(math.Pow(float64(10), float64(9)))) + 7)
-
-		}
-		r++
-
+	res := 0
+	pre := 0
+	mod := int(math.Pow(float64(10), float64(9))) + 7
+	for _, x := range nums {
+		res = (res + (x*x%mod)*x%mod + (x*x%mod)*pre%mod) % mod
+		pre = (pre*2 + x) % mod
 	}
-	return (power)
+
+	return res
 }
 
 func main() {
 	nums := []int{658, 489, 777, 2418, 1893, 130, 2448, 178, 1128, 2149, 1059, 1495, 1166, 608, 2006, 713, 1906, 2108, 680, 1348, 860, 1620, 146, 2447, 1895, 1083, 1465, 2351, 1359, 1187, 906, 533, 1943, 1814, 1808, 2065, 1744, 254, 1988, 1889, 1206}
 	// target := 5
+	// nums := []int{1, 2, 3, 4, 5, 6}
 	fmt.Println(sumOfPower(nums))
 }
 
